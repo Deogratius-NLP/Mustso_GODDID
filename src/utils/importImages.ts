20614b0a868c1fbb25fe791b642d9e3ccc8c1f80
Deogratius Kashumba <deogratiuskashumba@gmail.com>
2025-12-21 14:52:46 +0300

    Delete src/utils directory

--- src/utils/importImages.ts
+++
@@ -1,9 +0,0 @@
-// src/utils/importImages.ts
-export const importAllImages = (r: __WebpackModuleApi.RequireContext) => {
-  const images: Record<string, string> = {};
-  r.keys().forEach((key) => {
-    const fileName = key.replace('./', ''); // remove './' from './Presda.jpg'
-    images[fileName] = r(key).default;     // r(key) gives module object, .default is the actual URL
-  });
-  return images;
-};
